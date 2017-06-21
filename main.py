85c7837278a1fbd23359c2d9c5224dea31939fba
caichi <big_pineapple@126.com>
2017-06-21 22:43:07 +0800

    daily commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import time
+import json
 import math
 import logging
 import logging.handlers
@@ -54,10 +55,10 @@ def Prepare():
 def ConnectDB():
     return MySQLdb.connect(config.DB_HOST, config.DB_USER, config.DB_PASSWD, config.DB_NAME, config.DB_PORT)
 
-# CreateUser
+# create_user
 @app.route('/users', methods = ['POST'])
-def CreateUser():
-    app.logger.info('CreateUser')
+def create_user():
+    app.logger.info('create_user')
     try:
 		if not request.is_json():
 			return make_response('request data must be json-formatted.', 400)
@@ -66,7 +67,7 @@ def CreateUser():
     	try:
 			d_map = request.get_json(force = True)
     	except Exception, e:
-    	    app.logger.exception('CreateUserFail: %s' % str(e))
+    	    app.logger.exception('create user fail: %s' % str(e))
 			return make_response('fail to decode request data', 400)
 		
 		if 0 == len(d_map):
@@ -75,24 +76,94 @@ def CreateUser():
     	db = ConnectDB()
     	try:
     	    cur = db.cursor()
-			sql = 'insert into user(uid, info) values(%s, %s)' % 
+			sql = "insert into user(uid, info) values('%d', '%s')" % 
     	    cur.execute(sql, (d_map['Uid'], json.dumps(d_map['Info'])))
     	    db.commit()
     	except (MySQLdb.Warning, MySQLdb.Error) as e:
     	    db.rollback()
-    	    app.logger.exception('CreateUserFail: %s' % str(e))
+    	    app.logger.exception('create user fail: %s' % str(e))
 			return make_response('internal error', 500)
     	except Exception, e:
-    	    app.logger.exception('CreateUserFail: %s' % str(e))
+    	    app.logger.exception('create user fail: %s' % str(e))
 			return make_response('internal error', 500)
     	finally:
     	    db.close()
 		return make_response('', 201)
     except Exception, e:
-        app.logger.exception('CreateUserFail: %s' % str(e))
+        app.logger.exception('create user fail: %s' % str(e))
 	return make_response('internal error', 500)
 
-       
+def is_existed_user(uid):
+	db = ConnectDB()
+	try:
+	    cur = db.cursor()
+		sql = "select uid from user where uid = '%d'" 
+	    cur.execute(sql, (uid))
+		data = cur.fetchone()
+		if data:
+			return True
+		return False
+	except Exception, e:
+	    app.logger.exception('is_existed_user: %s' % str(e))
+	  	raise
+	finally:
+	    db.close()
+
+def out_of_service(uid, paper_id):
+	db = ConnectDB()
+	try:
+	    cur = db.cursor()
+		sql = "select info from billing where uid = '%d'" 
+	    cur.execute(sql, (uid))
+		billing_info = json.loads(cur.fetchone())
+		
+		sql = "select create_time from test_paper where id = '%d'" 
+	    cur.execute(sql, (paper_id))
+		create_time = cur.fetchone()
+		if billing_info['ExpireTime'] >= create_time:
+			return False
+		return True
+	except Exception, e:
+	    app.logger.exception('out_of_service: %s' % str(e))
+	  	raise
+	finally:
+	    db.close()
+
+# get_testpaper 
+@app.route('/testpapers/<paper_id>', methods = ['GET'])
+def get_testpaper(paper_id):
+    app.logger.info('get testpaper')
+    
+	try:
+		# check user credentials
+		if 'Uid' not in request.headers:
+			return make_response('Uid should be included in request headers.', 400)
+		
+		if not isinstance(request.headers['Uid'], int):
+			return make_response('Uid should be int type.', 400)
+
+		if not is_existed_user(request.headers['Uid']):
+			return make_response('%s is not valid user.' % request.headers['Uid'], 403)
+		
+		# check user billing
+		if out_of_service(request.headers['Uid'], paper_id):
+			return make_response('%s is out of service, please recharge later.' % request.headers['Uid'], 403)
+    	
+		#db = ConnectDB()
+    	#try:
+    	#    cur = db.cursor()
+		#	sql = 'select count() from user where uid = %s' 
+    	#    cur.execute(sql, (request.headers['Uid']))
+		#	data = cur.fetchone()
+    	#except (MySQLdb.Warning, MySQLdb.Error) as e:
+    	#    db.rollback()
+    	#    app.logger.exception('create user fail: %s' % str(e))
+		#	return make_response('internal error', 500)
+    	#except Exception, e:
+    	#    app.logger.exception('create user fail: %s' % str(e))
+		#	return make_response('internal error', 500)
+    	#finally:
+    	#    db.close()
 
 if __name__ == '__main__':
     port = 2469
